0f47557976bfcf699e468ed5c5c367ccf0be147f
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-03-18 23:07:00 -0400

    day 1

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,90 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
 
-
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  if 'butter' in ingredients  :
+    print("true")
+  else:
+    ingredients.update({'butter':500000})
+  if 'butter' in recipe  :
+    print("true")
+  else:
+    recipe.update({'butter':5000})
+    # setattr(ingredients, butter, 0)
+  if ingredients['butter'] == 500000 and recipe['butter'] < 5000:
+    return 0
+  if recipe['butter'] == 5000 and ingredients['butter'] < 5000:
+    return 0
+  if 'milk' in ingredients  :
+    print("true")
+  else:
+    ingredients.update({'milk':500000})
+  if 'milk' in recipe  :
+    print("true")
+  else:
+    recipe.update({'milk':5000})
+    # setattr(ingredients, butter, 0)
+  if ingredients['milk'] == 500000 and recipe['milk'] < 5000:
+    return 0
+  if recipe['milk'] == 5000 and ingredients['milk'] < 5000:
+    return 0
+  if 'cheese' in ingredients  :
+    print("true")
+  else:
+    ingredients.update({'cheese':500000})
+  if 'cheese' in recipe  :
+    print("true")
+  else:
+    recipe.update({'cheese':5000})
+    # setattr(ingredients, butter, 0)
+  if ingredients['cheese'] == 500000 and recipe['cheese'] < 5000:
+    return 0
+  if recipe['cheese'] == 5000 and ingredients['cheese'] < 5000:
+    return 0
+  if 'sugar' in ingredients  :
+    print("true")
+  else:
+    ingredients.update({'sugar':500000})
+  if 'sugar' in recipe  :
+    print("true")
+  else:
+    recipe.update({'sugar':5000})
+    # setattr(ingredients, butter, 0)
+  if ingredients['sugar'] == 500000 and recipe['sugar'] < 5000:
+    return 0
+  if recipe['sugar'] == 5000 and ingredients['sugar'] < 5000:
+    return 0
+  if 'flour' in ingredients  :
+    print("true")
+  else:
+    ingredients.update({'flour':500000})
+  if 'flour' in recipe  :
+    print("true")
+  else:
+    recipe.update({'flour':5000})
+    # setattr(ingredients, butter, 0)
+  if ingredients['flour'] == 500000 and recipe['flour'] < 5000:
+    return 0
+  if recipe['flour'] == 5000 and ingredients['flour'] < 5000:
+    return 0
+  cheese = round(ingredients['cheese'] / recipe['cheese'])
+  sugar = round(ingredients['sugar'] / recipe['sugar'])
+  flour = round(ingredients['flour'] / recipe['flour'])
+  milk = round(ingredients['milk'] / recipe['milk'])
+  butter = round(ingredients['butter'] / recipe['butter'])
+  array = [cheese,sugar,flour,milk,butter]
+  return min(array)
+    
+    
+  
+  
+  
+  
+  
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
 
 import argparse
+def compare(num1,num2):
+  if num1 > num2:
+    return num1
+  else:
+    return num2
+def findMaxRec(A,n): 
+    # if n = 0 means whole array  
+    # has been traversed 
+    if (n == 1): 
+        return A[0] 
+    return max(A[n - 1], findMaxRec(A, n - 1)) 
+def findMinRec(A,n): 
+  
+    # if n = 0 means whole array  
+    # has been traversed 
+  if (n == 1): 
+      return A[0] 
+  return min(A[n - 1], findMinRec(A, n - 1)) 
 
-def find_max_profit(prices):
-  pass
+def find_max_profit(arr):
+  max = findMaxRec(arr,len(arr))
+  index = arr.index(max)
+  if index == 0:
+    print(arr[2:])
+    max = findMaxRec(arr[2:],len(arr[2:]))
+    index = arr.index(max)
+  min_array = arr[:index]
+  min = findMinRec(min_array,len(min_array))
+  # min = findMinRec(arr[:arr.index(max)],len(arr[:arr.index(max)]))
+  # print(min)
+  return max - min 
 
-
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
-
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
